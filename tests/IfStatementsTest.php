6a81801c7290f6d4a33d4d36c651d71220fbe4c8
SerhiiCho <serhiicho@protonmail.com>
2019-09-23 00:37:27 +0300

    Added tests for if statements

--- tests/IfStatementsTest.php
+++ tests/IfStatementsTest.php
@@ -7,12 +7,24 @@ use Serhii\HtmlParser\Parser;
 
 class IfStatementsTest extends TestCase
 {
-    /** @test */
-    public function can_parse_if_statement_with_1_variable_and_1_line_inside_if_statement(): void
+    /**
+     * @dataProvider DataProvider_for_can_parse_if_statement
+     * @test
+     */
+    public function can_parse_if_statement($expect, $file_name, $boolean): void
     {
-        $expect = "<div></div>\n<h1>Print this</h1>\n<div></div>";
-        $parser = new Parser(get_path('if/state-with-var'), ['is_true' => true]);
+        $parser = new Parser(get_path("if/$file_name"), compact('boolean'));
 
         $this->assertEquals($expect, $parser->parseHtml());
+    }
+
+    public function DataProvider_for_can_parse_if_statement(): array
+    {
+        return [
+            ["<div></div>\n<h1>Print this</h1>\n<div></div>", '1-line-content', true],
+            ["<div></div>\n\n<div></div>", '1-line-content', false],
+            ["<div></div>\n<h1>Print this</h1>\n<p>Content</p>\n<div></div>", '2-lines-content', true],
+            ["<div></div>\n\n<div></div>", '2-lines-content', false],
+        ];
     }
 }
