365d6d8224711497c1a674c4f932f887f817ad8e
SerhiiCho <serhiicho@protonmail.com>
2020-02-14 07:07:30 +0200

    Added regex for if/else block

--- src/regex.php
+++ src/regex.php
@@ -4,5 +4,6 @@ return (object) [
 
     'match_variables' => '/{{ ?\$([_a-z0-9]+)? ?}}/',
     'match_if_statements' => '/{{ ?if ?\$([_A-z0-9]+) ?}}([\s\S]+?){{ ?end ?}}/',
+    'match_if_else_statements' => '/{{ ?if ?\$([_A-z0-9]+) ?}}([\s\S]+?){{ ?else ?}}([\s\S]+?){{ ?end ?}}/',
 
 ];
