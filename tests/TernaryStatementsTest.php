dbd8d4e8e3671ac782d4ec3070082671b4850af8
serhii <serhiicho@protonmail.com>
2020-11-17 13:53:01 +0200

    Removed test that doesn't belong to a test case

--- tests/TernaryStatementsTest.php
+++ tests/TernaryStatementsTest.php
@@ -33,24 +33,4 @@ class TernaryStatementsTest extends TestCase
             ["<h1>Do not print this</h1>", '1-line-content-double-quotes', false],
         ];
     }
-
-    /**
-     * @dataProvider DataProvider_for_can_parse_if_statement_when_has_another_variable_inside
-     * @test
-     */
-    public function can_parse_if_statement_when_has_another_variable_inside($expect, $file_name, $boolean, $content, $content2): void
-    {
-        $parser = new Parser(self::getPath("ifelse/$file_name"), compact('boolean', 'content', 'content2'));
-        $this->assertEquals($expect, $parser->parseHtml());
-    }
-
-    public function DataProvider_for_can_parse_if_statement_when_has_another_variable_inside(): array
-    {
-        return [
-            ["<div></div>\nSome text is here\n<div></div>", 'var-in-if', true, 'Some text is here', 'Another text'],
-            ["<div></div>\nAnother text\n<div></div>", 'var-in-if', false, 'Some text is here', 'Another text'],
-            ["<div></div>\nContent\n    Content\n<div></div>", 'var-in-if-2-lines', true, 'Content', 'Other'],
-            ["<div></div>\nOther\n    Other\n<div></div>", 'var-in-if-2-lines', false, 'Content', 'Other'],
-        ];
-    }
 }
