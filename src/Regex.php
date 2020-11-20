a7a822ec3945fcf278a6cd7a1d4ad4fe77126176
serhii <serhiicho@protonmail.com>
2020-11-20 17:21:06 +0200

    Improved regex for block if statements

--- src/Regex.php
+++ src/Regex.php
@@ -7,7 +7,8 @@ namespace Serhii\GoodbyeHtml;
 final class Regex
 {
     public const VARIABLES = '/{{ ?\$([_a-z0-9]+)? ?}}/u';
-    public const IF_STATEMENTS= '/{{ ?if ?\$([_A-z0-9]+) ?}}(((?!{{ ?else ?}})(?!{{ ?end ?}})[\s\S])*){{ ?end ?}}/u';
+    public const BLOCK_IF_STATEMENTS= '/([^\S\n]+)?{{ ?if ?\$([_A-z0-9]+) ?}}\n([\s\S]+?)([^\S\n]+)?{{ ?end ?}}\n?/u';
+    public const INLINE_IF_STATEMENTS= '/{{ ?if ?\$([_A-z0-9]+) ?}}(.*?){{ ?end ?}}/u';
     public const IF_ELSE_STATEMENTS = '/{{ ?if ?\$([_A-z0-9]+) ?}}((?!{{ ?end ?}})[\s\S])*{{ ?else ?}}([\s\S]+?){{ ?end ?}}/u';
     public const TERNARY_VARIABLES = '/{{ ?\$([_A-z0-9]+) ?\? ?((\$([_a-z0-9]+)?)|(.*?)) ?: ?((\$([_a-z0-9]+)?)|(.*?)) ?}}/u';
     public const TERNARY_STATEMENTS = '/{{ ?\$([_A-z0-9]+) ?\? ?((\'|")(.*?)(\'|")) ?: ?(\'|")(.*?)(\'|") ?}}/u';
--- src/Syntax/ReplacesIf.php
+++ src/Syntax/ReplacesIf.php
@@ -10,14 +10,17 @@ trait ReplacesIf
 {
     private function replaceIfStatementsFromHtml(): void
     {
-        preg_match_all(Regex::IF_STATEMENTS, $this->html_content, $matches);
+        preg_match_all(Regex::BLOCK_IF_STATEMENTS, $this->html_content, $block_matches);
+        preg_match_all(Regex::INLINE_IF_STATEMENTS, $this->html_content, $inline_matches);
 
-        [$raw, $var_names, $contents] = $matches;
+        $raw = array_merge($inline_matches[0], $block_matches[0]);
+        $var_names = array_merge($inline_matches[1], $block_matches[2]);
+        $contents = array_merge($inline_matches[2], $block_matches[3]);
 
         $replacements = [];
 
         for ($i = 0; $i < count($raw); $i++) {
-            $replacements[] = $this->variables[$var_names[$i]] ? trim($contents[$i]) : '';
+            $replacements[] = $this->variables[$var_names[$i]] ? $contents[$i] : '';
         }
 
         $this->replaceStatements(compact('raw', 'replacements', 'var_names'));
