643fe530f82a779881e5c9a2cd08ddfb0eebbd00
serhii <serhiicho@protonmail.com>
2020-11-20 16:58:37 +0200

    Changed Regex.php

--- src/Regex.php
+++ src/Regex.php
@@ -7,8 +7,8 @@ namespace Serhii\GoodbyeHtml;
 final class Regex
 {
     public const VARIABLES = '/{{ ?\$([_a-z0-9]+)? ?}}/u';
-    public const IF_STATEMENTS= '/{{ ?if ?\$([_A-z0-9]+) ?}}([\s\S]+?){{ ?end ?}}/u';
-    public const IF_ELSE_STATEMENTS = '/{{ ?if ?\$([_A-z0-9]+) ?}}([\s\S]+?){{ ?else ?}}([\s\S]+?){{ ?end ?}}/u';
+    public const IF_STATEMENTS= '/{{ ?if ?\$([_A-z0-9]+) ?}}(((?!{{ ?else ?}})(?!{{ ?end ?}})[\s\S])*){{ ?end ?}}/u';
+    public const IF_ELSE_STATEMENTS = '/{{ ?if ?\$([_A-z0-9]+) ?}}((?!{{ ?end ?}})[\s\S])*{{ ?else ?}}([\s\S]+?){{ ?end ?}}/u';
     public const TERNARY_VARIABLES = '/{{ ?\$([_A-z0-9]+) ?\? ?((\$([_a-z0-9]+)?)|(.*?)) ?: ?((\$([_a-z0-9]+)?)|(.*?)) ?}}/u';
     public const TERNARY_STATEMENTS = '/{{ ?\$([_A-z0-9]+) ?\? ?((\'|")(.*?)(\'|")) ?: ?(\'|")(.*?)(\'|") ?}}/u';
     public const BLOCK_LOOP = '/([^\S\n]+)?{{ ?loop ([0-9]+), ?([0-9]+) ?}}\n([\s\S]+?)([^\S\n]+)?{{ ?end ?}}\n?/u';
