b2f8e14eea04a65e562c658bd161fe2da6aff1d0
SerhiiCho <serhiicho@protonmail.com>
2023-10-26 20:39:18 +0300

    Removed methods from TestCase.php

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,16 +6,4 @@ namespace Serhii\Tests;
 
 class TestCase extends \PHPUnit\Framework\TestCase
 {
-    protected static function getPath(string $file_name): string
-    {
-        return __DIR__ . "/files/{$file_name}.html";
-    }
-
-    protected function getFileNames(string $blob): array
-    {
-        return array_map(static function ($name) {
-            $sections = explode('/', $name);
-            return [str_replace('.html', '', end($sections))];
-        }, glob(__DIR__ . $blob));
-    }
 }
