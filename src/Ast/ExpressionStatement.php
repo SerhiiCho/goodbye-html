2efaf7d9e9ffeb6f8b63cb3f79be7386b9f84287
SerhiiCho <serhiicho@protonmail.com>
2023-10-28 17:46:34 +0300

    WIP on expressions

---
+++ src/Ast/ExpressionStatement.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Serhii\GoodbyeHtml\Ast;
+
+use Serhii\GoodbyeHtml\Token\Token;
+
+readonly class ExpressionStatement implements Statement
+{
+    public function __construct(public Token $token, public Expression|null $expression)
+    {
+    }
+
+    public function tokenLiteral(): string
+    {
+        return $this->token->literal;
+    }
+
+    public function string(): string
+    {
+        if (!$this->expression) {
+            return '';
+        }
+
+        return $this->expression->string();
+    }
+}
--- src/Parser/Parser.php
+++ src/Parser/Parser.php
@@ -4,14 +4,30 @@ declare(strict_types=1);
 
 namespace Serhii\GoodbyeHtml\Parser;
 
+use Serhii\GoodbyeHtml\Ast\Expression;
+use Serhii\GoodbyeHtml\Ast\ExpressionStatement;
 use Serhii\GoodbyeHtml\Ast\Program;
 use Serhii\GoodbyeHtml\Ast\Statement;
 use Serhii\GoodbyeHtml\Lexer\Lexer;
+use Serhii\GoodbyeHtml\Token\Token;
+use Serhii\GoodbyeHtml\Token\TokenType;
 
 final readonly class Parser
 {
+    private Token $curToken;
+    private Token $peekToken;
+
+    /**
+     * @var string[]
+     */
+    private array $errors;
+
     public function __construct(private Lexer $lexer)
     {
+        $this->errors = [];
+
+        $this->nextToken();
+        $this->nextToken();
     }
 
     public function parseProgram(): Program
@@ -19,13 +35,55 @@ final readonly class Parser
         /** @var Statement[] $statements */
         $statements = [];
 
-        //
+        while (!$this->curTokenIs(TokenType::EOF)) {
+            $stmt = $this->parseStatement();
+
+            if ($stmt !== null) {
+                $statements[] = $stmt;
+            }
+
+            $this->nextToken();
+        }
 
         return new Program($statements);
     }
 
+    private function nextToken(): void
+    {
+        $this->curToken = $this->peekToken;
+        $this->peekToken = $this->lexer->nextToken();
+    }
+
     private function parseStatement(): Statement|null
     {
-        return null;
+        return match($this->curToken->type) {
+            default => $this->parseExpressionStatement(),
+        };
+    }
+
+    private function parseExpressionStatement(): ExpressionStatement
+    {
+        $expr = $this->parseExpression();
+        $result = new ExpressionStatement($this->curToken, $expr);
+
+        if ($this->peekTokenIs(TokenType::CLOSING_BRACES)) {
+            $this->nextToken();
+        }
+
+        return $result;
+    }
+
+    private function parseExpression(): Expression|null
+    {
+    }
+
+    private function curTokenIs(TokenType $token): bool
+    {
+        return $token === $this->curToken->type;
+    }
+
+    private function peekTokenIs(TokenType $token): bool
+    {
+        return $token === $this->peekToken->type;
     }
 }
