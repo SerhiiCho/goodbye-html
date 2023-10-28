0e23bc8a14d796940d18bd726958996769b69d3f
SerhiiCho <serhiicho@protonmail.com>
2023-10-28 17:55:35 +0300

    Added todos

--- src/Parser/Parser.php
+++ src/Parser/Parser.php
@@ -4,10 +4,12 @@ declare(strict_types=1);
 
 namespace Serhii\GoodbyeHtml\Parser;
 
+use Closure;
 use Serhii\GoodbyeHtml\Ast\Expression;
 use Serhii\GoodbyeHtml\Ast\ExpressionStatement;
 use Serhii\GoodbyeHtml\Ast\Program;
 use Serhii\GoodbyeHtml\Ast\Statement;
+use Serhii\GoodbyeHtml\Ast\VariableExpression;
 use Serhii\GoodbyeHtml\Lexer\Lexer;
 use Serhii\GoodbyeHtml\Token\Token;
 use Serhii\GoodbyeHtml\Token\TokenType;
@@ -16,6 +18,8 @@ final readonly class Parser
 {
     private Token $curToken;
     private Token $peekToken;
+    private array $prefixParseFns;
+    private array $infixParseFns;
 
     /**
      * @var string[]
@@ -25,9 +29,13 @@ final readonly class Parser
     public function __construct(private Lexer $lexer)
     {
         $this->errors = [];
+        $this->prefixParseFns = [];
+        $this->infixParseFns = [];
 
         $this->nextToken();
         $this->nextToken();
+
+        $this->registerPrefix(TokenType::VARIABLE, fn () => $this->parseVariable());
     }
 
     public function parseProgram(): Program
@@ -54,6 +62,16 @@ final readonly class Parser
         $this->peekToken = $this->lexer->nextToken();
     }
 
+    private function registerPrefix(TokenType $token, Closure $fn): void
+    {
+        $this->prefixParseFns[$token->value] = $fn;
+    }
+
+    private function registerInfix(TokenType $token, Closure $fn): void
+    {
+        $this->infixParseFns[$token->value] = $fn;
+    }
+
     private function parseStatement(): Statement|null
     {
         return match($this->curToken->type) {
@@ -75,6 +93,16 @@ final readonly class Parser
 
     private function parseExpression(): Expression|null
     {
+        $prefix = $this->prefixParseFns[$this->curToken->type->value] ?? null;
+
+        if ($prefix === null) {
+            // todo: error
+            return null;
+        }
+
+        $leftExp = $prefix();
+
+        // todo: here
     }
 
     private function curTokenIs(TokenType $token): bool
@@ -85,5 +113,13 @@ final readonly class Parser
     private function peekTokenIs(TokenType $token): bool
     {
         return $token === $this->peekToken->type;
+    }
+
+    private function parseVariable(): Expression
+    {
+        return new VariableExpression(
+            $this->curToken,
+            $this->curToken->literal,
+        );
     }
 }
