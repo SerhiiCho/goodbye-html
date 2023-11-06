1ff89abf93ca2909ea4c9fe23a134a3a6f950289
SerhiiCho <serhiicho@protonmail.com>
2023-11-06 18:06:05 +0200

    Fix formatting issue in IfExpression.php Added new lines to each block statement

--- src/Ast/IfExpression.php
+++ src/Ast/IfExpression.php
@@ -26,14 +26,14 @@ readonly class IfExpression implements Expression
         $result = sprintf("{{ if %s }}\n", $this->condition->string());
 
         foreach ($this->consequence->statements as $stmt) {
-            $result .= $stmt->string();
+            $result .= $stmt->string() . "\n";
         }
 
         if ($this->alternative) {
             $result .= "{{ else }}\n";
 
             foreach ($this->alternative->statements as $stmt) {
-                $result .= $stmt->string();
+                $result .= $stmt->string() + "\n";
             }
         }
 
