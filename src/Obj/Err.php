c2b424c911b3ec14ead999c69f01c844eb989a52
SerhiiCho <serhiicho@protonmail.com>
2023-11-07 11:38:14 +0200

    Add Err class implementation.

---
+++ src/Obj/Err.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Serhii\GoodbyeHtml\Obj;
+
+readonly class Err implements Obj
+{
+    public function __construct(public string $message)
+    {
+    }
+
+    public function type(): ObjType
+    {
+        return ObjType::ERROR_OBJ;
+    }
+
+    public function inspect(): string
+    {
+        return "ERROR: {$this->message}";
+    }
+}
