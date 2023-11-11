5b92614de32388ec6026158f556ffad749a4d291
SerhiiCho <serhiicho@protonmail.com>
2023-11-11 13:56:13 +0200

    Refactor LexerTest.php to include new test cases

--- tests/LexerTest.php
+++ tests/LexerTest.php
@@ -14,13 +14,14 @@ class LexerTest extends TestCase
     {
         $lexer = new Lexer($input);
 
-        foreach ($expect as $expectToken) {
+        foreach ($expect as $key => $expectToken) {
             $actualToken = $lexer->nextToken();
 
             $msg = sprintf(
-                "Expected token type: \n%s\ngot: \n%s",
+                "Expected token type: \n%s\ngot: \n%s\nCase #%d",
                 json_encode($expectToken, JSON_PRETTY_PRINT),
                 json_encode($actualToken, JSON_PRETTY_PRINT),
+                $key + 1,
             );
 
             $this->assertEquals($expectToken, $actualToken, $msg);
@@ -215,6 +216,69 @@ class LexerTest extends TestCase
             new Token(TokenType::STRING, "secondary-page"),
             new Token(TokenType::CLOSING_BRACES, "}}"),
             new Token(TokenType::HTML, "\">Hello world!</h3>"),
+            new Token(TokenType::EOF, ""),
+        ]);
+    }
+
+    public function testHtmlWhitespaceIsNotRemovedAfterLexing(): void
+    {
+        $input = <<<HTML
+        <body>
+            <section>
+                {{ if true }}
+                    <h1>Hello world!</h1>
+                {{ else }}
+                    <h1>Bye bye!</h1>
+                {{ end }}
+            </section>
+        </body>
+        HTML;
+
+        $this->tokenizeString($input, [
+            new Token(TokenType::HTML, "<body>\n    <section>\n        "),
+            new Token(TokenType::OPENING_BRACES, "{{"),
+            new Token(TokenType::IF, "if"),
+            new Token(TokenType::TRUE, "true"),
+            new Token(TokenType::CLOSING_BRACES, "}}"),
+            new Token(TokenType::HTML, "\n            <h1>Hello world!</h1>\n        "),
+            new Token(TokenType::OPENING_BRACES, "{{"),
+            new Token(TokenType::ELSE, "else"),
+            new Token(TokenType::CLOSING_BRACES, "}}"),
+            new Token(TokenType::HTML, "\n            <h1>Bye bye!</h1>\n        "),
+            new Token(TokenType::OPENING_BRACES, "{{"),
+            new Token(TokenType::END, "end"),
+            new Token(TokenType::CLOSING_BRACES, "}}"),
+            new Token(TokenType::HTML, "\n    </section>\n</body>"),
+            new Token(TokenType::EOF, ""),
+        ]);
+    }
+
+    public function testLexingCorrectlyWithoutHtml(): void
+    {
+        $input = <<<HTML
+        {{ if \$hasName }}{{ \$name }}{{ end }}
+        {{ \$isAdult ? 'Adult' : 'Child' }}
+        HTML;
+
+        $this->tokenizeString($input, [
+            new Token(TokenType::OPENING_BRACES, "{{"),
+            new Token(TokenType::IF, "if"),
+            new Token(TokenType::VARIABLE, "hasName"),
+            new Token(TokenType::CLOSING_BRACES, "}}"),
+            new Token(TokenType::OPENING_BRACES, "{{"),
+            new Token(TokenType::VARIABLE, "name"),
+            new Token(TokenType::CLOSING_BRACES, "}}"),
+            new Token(TokenType::OPENING_BRACES, "{{"),
+            new Token(TokenType::END, "end"),
+            new Token(TokenType::CLOSING_BRACES, "}}"),
+            new Token(TokenType::HTML, "\n"), // <-- The only HTML token
+            new Token(TokenType::OPENING_BRACES, "{{"),
+            new Token(TokenType::VARIABLE, "isAdult"),
+            new Token(TokenType::QUESTION_MARK, "?"),
+            new Token(TokenType::STRING, "Adult"),
+            new Token(TokenType::COLON, ":"),
+            new Token(TokenType::STRING, "Child"),
+            new Token(TokenType::CLOSING_BRACES, "}}"),
             new Token(TokenType::EOF, ""),
         ]);
     }
