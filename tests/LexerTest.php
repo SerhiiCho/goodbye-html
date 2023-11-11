3dd6dc6a0a5de52f06f9cfc8a7bed44f8502b624
SerhiiCho <serhiicho@protonmail.com>
2023-11-11 13:39:22 +0200

    rewrite lexer tests

--- tests/LexerTest.php
+++ tests/LexerTest.php
@@ -10,192 +10,137 @@ use Serhii\GoodbyeHtml\Token\TokenType;
 
 class LexerTest extends TestCase
 {
-    public function testLexer(): void
+    public function testLexingStrings(): void
     {
         $input = <<<HTML
-        <div class="{{ \$show20 ? 'container' : '' }}">
-        <h1 {{ \$classes }}>{{ \$heading }}</h1>
-
-        <ul {{ "data-attr" }}>
-        {{    loop 1, \$to     }}
-            <li>{{ \$index }}</li>
-        {{ end }}
-        </ul>
-
-        {{ if \$is_old }}
-            <h2>I'm not a pro but it's only a matter of time</h2>
-        {{ end }}
+        <div>
+            <h2>{{ 'Hello world!' }}</h2>
+            <h3>{{ "Good luck!" }}</h3>
         </div>
+        HTML;
 
-        {{ if false }}
-            <h3>I like bread</h3>
-        {{ else }}
-            <h4>I don't really like bread</h4>
-        {{ end }}
+        $expect = [
+            new Token(TokenType::HTML, "<div>\n    <h2>"),
+            new Token(TokenType::OPENING_BRACES, "{{"),
+            new Token(TokenType::STRING, "Hello world!"),
+            new Token(TokenType::CLOSING_BRACES, "}}"),
+            new Token(TokenType::HTML, "</h2>\n    <h3>"),
+            new Token(TokenType::OPENING_BRACES, "{{"),
+            new Token(TokenType::STRING, "Good luck!"),
+            new Token(TokenType::CLOSING_BRACES, "}}"),
+            new Token(TokenType::HTML, "</h3>\n</div>"),
+            new Token(TokenType::EOF, ""),
+        ];
 
-        <h5>{{ if true }}{{ \$cat_var }}{{ else }}{{ \$dog_var }}{{ end }}</h5>
+        $this->tokenizeString($input, $expect);
+    }
 
-        {{ if -3 }}
-            You are a cool {{ if \$male }}guy{{ end }}
+    public function testLexingIntegers(): void
+    {
+        $input = <<<HTML
+        <h1>We have {{ 3 }} computers</h1>
+        HTML;
+
+        $expect = [
+            new Token(TokenType::HTML, "<h1>We have "),
+            new Token(TokenType::OPENING_BRACES, "{{"),
+            new Token(TokenType::INTEGER, "3"),
+            new Token(TokenType::CLOSING_BRACES, "}}"),
+            new Token(TokenType::HTML, " computers</h1>"),
+            new Token(TokenType::EOF, ""),
+        ];
+
+        $this->tokenizeString($input, $expect);
+    }
+
+    public function testLexingBooleans(): void
+    {
+        $input = <<<HTML
+        <h1>{{ true }} and {{ false }}</h1>
+        HTML;
+
+        $expect = [
+            new Token(TokenType::HTML, "<h1>"),
+            new Token(TokenType::OPENING_BRACES, "{{"),
+            new Token(TokenType::TRUE, "true"),
+            new Token(TokenType::CLOSING_BRACES, "}}"),
+            new Token(TokenType::HTML, " and "),
+            new Token(TokenType::OPENING_BRACES, "{{"),
+            new Token(TokenType::FALSE, "false"),
+            new Token(TokenType::CLOSING_BRACES, "}}"),
+            new Token(TokenType::HTML, "</h1>"),
+            new Token(TokenType::EOF, ""),
+        ];
+
+        $this->tokenizeString($input, $expect);
+    }
+
+    public function testLexingIfExpressions(): void
+    {
+        $input = <<<HTML
+        {{ if true }}
+            <h1>Hello world!</h1>
         {{ end }}
         HTML;
 
-        $tests = [
-            new Token(TokenType::HTML, "<div class=\""),
+        $expect = [
+            new Token(TokenType::OPENING_BRACES, "{{"),
+            new Token(TokenType::IF, "if"),
+            new Token(TokenType::TRUE, "true"),
+            new Token(TokenType::CLOSING_BRACES, "}}"),
+            new Token(TokenType::HTML, "\n    <h1>Hello world!</h1>\n"),
+            new Token(TokenType::OPENING_BRACES, "{{"),
+            new Token(TokenType::END, "end"),
+            new Token(TokenType::CLOSING_BRACES, "}}"),
+            new Token(TokenType::EOF, ""),
+        ];
 
-            // Coalescing operator
-            new Token(TokenType::OPENING_BRACES, "{{"),
-            new Token(TokenType::VARIABLE, "show20"),
-            new Token(TokenType::QUESTION_MARK, "?"),
-            new Token(TokenType::STRING, "container"),
-            new Token(TokenType::COLON, ":"),
-            new Token(TokenType::STRING, ""),
-            new Token(TokenType::CLOSING_BRACES, "}}"),
-            // End coalescing operator
+        $this->tokenizeString($input, $expect);
+    }
 
-            // Variables
-            new Token(TokenType::HTML, "\">\n<h1 "),
-            new Token(TokenType::OPENING_BRACES, "{{"),
-            new Token(TokenType::VARIABLE, "classes"),
-            new Token(TokenType::CLOSING_BRACES, "}}"),
-            new Token(TokenType::HTML, ">"),
-            new Token(TokenType::OPENING_BRACES, "{{"),
-            new Token(TokenType::VARIABLE, "heading"),
-            new Token(TokenType::CLOSING_BRACES, "}}"),
-            new Token(TokenType::HTML, "</h1>\n\n<ul "),
-            new Token(TokenType::OPENING_BRACES, "{{"),
-            new Token(TokenType::STRING, "data-attr"),
-            new Token(TokenType::CLOSING_BRACES, "}}"),
-            new Token(TokenType::HTML, ">\n"),
-            // End variables
+    public function testLexingLoopExpressions(): void
+    {
+        $input = <<<HTML
+        <ul>
+            {{ loop 1, 4 }}
+                <li>item</li>
+            {{ end }}
+        </ul>
+        HTML;
 
-            // Loop statement
+        $expect = [
+            new Token(TokenType::HTML, "<ul>\n    "),
             new Token(TokenType::OPENING_BRACES, "{{"),
             new Token(TokenType::LOOP, "loop"),
             new Token(TokenType::INTEGER, "1"),
             new Token(TokenType::COMMA, ","),
-            new Token(TokenType::VARIABLE, "to"),
+            new Token(TokenType::INTEGER, "4"),
             new Token(TokenType::CLOSING_BRACES, "}}"),
-            new Token(TokenType::HTML, "<li>"),
-            new Token(TokenType::OPENING_BRACES, "{{"),
-            new Token(TokenType::VARIABLE, "index"),
-            new Token(TokenType::CLOSING_BRACES, "}}"),
-            new Token(TokenType::HTML, "</li>\n"),
-            new Token(TokenType::OPENING_BRACES, "{{"),
-            new Token(TokenType::END, "end"),
-            new Token(TokenType::CLOSING_BRACES, "}}"),
-            // End loop statement
-
-            new Token(TokenType::HTML, "</ul>\n\n"),
-
-            // If statement
-            new Token(TokenType::OPENING_BRACES, "{{"),
-            new Token(TokenType::IF, "if"),
-            new Token(TokenType::VARIABLE, "is_old"),
-            new Token(TokenType::CLOSING_BRACES, "}}"),
-            new Token(TokenType::HTML, "<h2>I'm not a pro but it's only a matter of time</h2>\n"),
-            new Token(TokenType::OPENING_BRACES, "{{"),
-            new Token(TokenType::END, "end"),
-            new Token(TokenType::CLOSING_BRACES, "}}"),
-            // End if statement
-
-            new Token(TokenType::HTML, "</div>\n\n"),
-
-            // If / Else statement
+            new Token(TokenType::HTML, "\n        <li>item</li>\n    "),
             new Token(TokenType::OPENING_BRACES, '{{'),
-            new Token(TokenType::IF, "if"),
-            new Token(TokenType::FALSE, "false"),
+            new Token(TokenType::END, "end"),
             new Token(TokenType::CLOSING_BRACES, '}}'),
-            new Token(TokenType::HTML, "<h3>I like bread</h3>\n"),
-            new Token(TokenType::OPENING_BRACES, '{{'),
-            new Token(TokenType::ELSE, "else"),
-            new Token(TokenType::CLOSING_BRACES, '}}'),
-            new Token(TokenType::HTML, "<h4>I don't really like bread</h4>\n"),
-            new Token(TokenType::OPENING_BRACES, "{{"),
-            new Token(TokenType::END, "end"),
-            new Token(TokenType::CLOSING_BRACES, "}}"),
-            // End If / Else statement
-
-            // Inline If / Else / End statement
-            new Token(TokenType::HTML, "<h5>"),
-            new Token(TokenType::OPENING_BRACES, "{{"),
-            new Token(TokenType::IF, "if"),
-            new Token(TokenType::TRUE, 'true'),
-            new Token(TokenType::CLOSING_BRACES, "}}"),
-            new Token(TokenType::OPENING_BRACES, "{{"),
-            new Token(TokenType::VARIABLE, 'cat_var'),
-            new Token(TokenType::CLOSING_BRACES, "}}"),
-            new Token(TokenType::OPENING_BRACES, "{{"),
-            new Token(TokenType::ELSE, 'else'),
-            new Token(TokenType::CLOSING_BRACES, "}}"),
-            new Token(TokenType::OPENING_BRACES, "{{"),
-            new Token(TokenType::VARIABLE, 'dog_var'),
-            new Token(TokenType::CLOSING_BRACES, "}}"),
-            new Token(TokenType::OPENING_BRACES, "{{"),
-            new Token(TokenType::END, "end"),
-            new Token(TokenType::CLOSING_BRACES, "}}"),
-            new Token(TokenType::HTML, "</h5>\n\n"),
-            // End inline If / Else / End statement
-
-            // Nested If statement
-            new Token(TokenType::OPENING_BRACES, "{{"),
-            new Token(TokenType::IF, 'if'),
-            new Token(TokenType::MINUS, '-'),
-            new Token(TokenType::INTEGER, '3'),
-            new Token(TokenType::CLOSING_BRACES, "}}"),
-            new Token(TokenType::HTML, 'You are a cool '),
-            new Token(TokenType::OPENING_BRACES, "{{"),
-            new Token(TokenType::IF, 'if'),
-            new Token(TokenType::VARIABLE, 'male'),
-            new Token(TokenType::CLOSING_BRACES, "}}"),
-            new Token(TokenType::HTML, 'guy'),
-            new Token(TokenType::OPENING_BRACES, "{{"),
-            new Token(TokenType::END, "end"),
-            new Token(TokenType::CLOSING_BRACES, "}}"),
-            new Token(TokenType::OPENING_BRACES, "{{"),
-            new Token(TokenType::END, "end"),
-            new Token(TokenType::CLOSING_BRACES, "}}"),
-            // End nested If statement
-
-            new Token(TokenType::EOF, 'EOF'),
+            new Token(TokenType::HTML, "\n</ul>"),
+            new Token(TokenType::EOF, ""),
         ];
 
-        $lexer = new Lexer($input);
-
-        foreach ($tests as $test) {
-            $token = $lexer->nextToken();
-            $this->assertEquals($test, $token, "Test for: {$test->literal}");
-            $this->assertEquals($test->literal, $token->literal, "Test for token type: {$token->type->value}");
-        }
+        $this->tokenizeString($input, $expect);
     }
 
-    public function testProgramCanBeEmptyString(): void
+    private function tokenizeString(string $input, array $expect): void
     {
-        $input = '';
-
-        $lexer = new Lexer($input);
-        $token = $lexer->nextToken();
-
-        $this->assertEquals(new Token(TokenType::EOF, 'EOF'), $token);
-    }
-
-    public function testProgramCanJustWithoutHtml(): void
-    {
-        $input = '{{ $name }}';
-
         $lexer = new Lexer($input);
 
-        $tests = [
-            new Token(TokenType::OPENING_BRACES, '{{'),
-            new Token(TokenType::VARIABLE, 'name'),
-            new Token(TokenType::CLOSING_BRACES, '}}'),
-            new Token(TokenType::EOF, 'EOF'),
-        ];
+        foreach ($expect as $expectToken) {
+            $actualToken = $lexer->nextToken();
 
-        foreach ($tests as $test) {
-            $token = $lexer->nextToken();
-            $this->assertEquals($test, $token, "Test for: {$test->literal}");
-            $this->assertEquals($test->literal, $token->literal, "Test for token type: {$token->type->value}");
+            $msg = sprintf(
+                "Expected token type: \n%s\ngot: \n%s",
+                json_encode($expectToken, JSON_PRETTY_PRINT),
+                json_encode($actualToken, JSON_PRETTY_PRINT),
+            );
+
+            $this->assertEquals($expectToken, $actualToken, $msg);
         }
     }
 }
